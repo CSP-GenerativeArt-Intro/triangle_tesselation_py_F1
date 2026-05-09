7956e7d38604271839916d3d9686e0b13136a74d
areidakpena-ssis <153045302+areidakpena-ssis@users.noreply.github.com>
2026-05-09 11:29:20 +0700

    Clean up structure of picture.py

--- picture.py
+++ picture.py
@@ -8,31 +8,36 @@ def draw_picture(width, height):
     
     # make some variables available
     colors = ["red", "green", "blue", "cyan", "magenta", "yellow"]
+
+    # sizes of triangles are based on canvas size
+    # we want to fit in 3 horizontally and 5 vertically.
+    TRIANGLE_HEIGHT = height / 5
+    TRIANGLE_WIDTH = width / 3
+
+    # ==================================================================
+    # Draw the tesselation (you should only edit code below this part!)
+    # ==================================================================
     
-    triangle_height = height/5
-    triangle_width = width / 3
+    # code for RED triangles
+    sg.set_fill_color(colors[0]) # set fill color to red
     
-    # Draw the tesselation
-    # code for red triangles
-    sg.set_fill_color(colors[0])
-    
-    for i in range(5):
-        sg.fill_triangle(2*triangle_width, i*triangle_height, 3*triangle_width, i*triangle_height, 3*triangle_width, (i+1)*triangle_height) 
+    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
+        sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
         
     
-    # code for blue triangles
+    # code for BLUE triangles
     
     
-    # code for yellow triangles
+    # code for YELLOW triangles
    
    
-    # code for green triangles
+    # code for GREEN triangles
  
  
-    # code for magenta triangles
+    # code for MAGENTA triangles
     
     
-    # code for cyan triangles
+    # code for CYAN triangles
     
     
     
